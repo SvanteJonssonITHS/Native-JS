14f00b165c57c6bd4efe28371bf2689d37e8dc51
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-21 09:15:51 +0200

    De fyra räknesätten

--- Operatorer-för-aritmetik-(och-strängsammanslagning)/index.js
+++ Operatorer-för-aritmetik-(och-strängsammanslagning)/index.js
@@ -1,5 +1,7 @@
-let one = 1;
-let two = "2";
+let n = 4;
+let o = 12;
+let p = 4;
 
-console.log(one + one)
-console.log(two + two)
+console.log(n + o - 1)
+console.log(o - p * 2)
+console.log(p / 4 === 1)
