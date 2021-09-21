83baad0c96bf2102d23b2886ec3de9775ee5b489
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-21 09:19:54 +0200

    Avdrag för moms: Del 1

--- Operatorer-för-aritmetik-(och-strängsammanslagning)/index.js
+++ Operatorer-för-aritmetik-(och-strängsammanslagning)/index.js
@@ -1,7 +1,2 @@
-let n = 4;
-let o = 12;
-let p = 4;
-
-console.log(n + o - 1)
-console.log(o - p * 2)
-console.log(p / 4 === 1)
+let price = prompt("Skriv in kostnaden:");
+window.alert(price * 0.8);
