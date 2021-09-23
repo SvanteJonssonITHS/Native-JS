c9a653bea821e3e375fdbb8de3f48b0dc0d9ccdb
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-23 09:20:15 +0200

    Avdrag för moms: Del 3

--- if-och-else-samt-switch/index.js
+++ if-och-else-samt-switch/index.js
@@ -1,2 +1,5 @@
 let price = prompt("Skriv in kostnaden:");
-if(price) window.alert(price * 0.8); 
+if(price){
+    if(!isNaN(price)) window.alert(price * 0.8); 
+    else window.alert("Du skrev inte in ett nummer.")
+} 
