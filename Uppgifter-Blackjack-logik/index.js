6ebb2787751950bc4563eebd4cf5ad9aaefed72e
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-29 16:52:11 +0200

    Dra ett kort

--- Uppgifter-Blackjack-logik/index.js
+++ Uppgifter-Blackjack-logik/index.js
@@ -1,10 +1,5 @@
-let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS']
-createDeck = () => {
-    let deck = []
-    for (let i = 0; i < suits.length; i++) {
-        for (let j = 1; j < 14; j++) {
-            deck.push({suit: suits[i], value: j })
-        }
-    }
-    return deck
+draw = (deck) => {
+    let card = deck[deck.length-1]
+    deck.pop()
+    return card
 }
