8dd11e9060b04e2f5839fc3774331af72e0aef84
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-29 17:35:43 +0200

    Blackjack-poäng: Del 2

--- Uppgifter-Blackjack-logik/index.js
+++ Uppgifter-Blackjack-logik/index.js
@@ -1,13 +1,25 @@
 score = (cards) => {
     let sum = 0
     for (let i = 0; i < cards.length; i++) {
-        if(cards[i].value == 1){
+        if (cards[i].value == 1) {
             sum += 11
-        } else if(cards[i].value >= 10){
+        } else if (cards[i].value >= 10) {
             sum += 10
         } else {
             sum += cards[i].value
         }
     }
+
+    if (sum > 21) {
+        while (true) {
+            if (cards.some(c => c.value == 1)) {
+                sum -= 10;
+            }
+            if (sum <= 21) {
+                break;
+            }
+        }
+    }
+
     return sum
 }
