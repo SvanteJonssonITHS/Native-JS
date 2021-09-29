86f19eb9efe4e4da333062c2efad8d78304bd38b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-09-29 16:47:52 +0200

    Strängrepresentation av ett objekt

--- Uppgifter-Blackjack-logik/index.js
+++ Uppgifter-Blackjack-logik/index.js
@@ -1,7 +1,27 @@
-let deck = []
-let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS']
-for (let i = 0; i < suits.length; i++) {
-    for (let j = 1; j < 14; j++) {
-        deck.push({suit: suits[i], value: j })
+let suits = {
+    'HEARTS': "♥",
+    'SPADES': "♠",
+    'DIAMONDS': "♦",
+    'CLUBS': "♣"
+}
+let numbers = {
+    1: "A",
+    10: "T",
+    11: "J",
+    12: "Q",
+    13: "K"
+}
+prettyCard = (card) => {
+    let pretty
+    if (numbers.hasOwnProperty(card.value)) {
+        pretty = numbers[card.value]
+    } else {
+        pretty = card.value
     }
+
+    if (suits.hasOwnProperty(card.suit)) {
+        pretty += suits[card.suit]
+    }
+    
+    return pretty
 }
