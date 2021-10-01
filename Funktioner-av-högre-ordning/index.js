4a5f8e1fe8d20a712a224ee08543d1bd8dd96e27
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-01 09:23:12 +0200

    find

--- Funktioner-av-högre-ordning/index.js
+++ Funktioner-av-högre-ordning/index.js
@@ -1,5 +1,9 @@
-forEach = (a, f) => {
+function f(n) {
+    return n > 2
+  }
+  
+  function find(a, g) {
     for (let i = 0; i < a.length; i++) {
-        f(a[i])        
+        if(g(a[i])) return a[i]        
     }
-}
+  }
