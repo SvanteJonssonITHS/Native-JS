715345c5d8f0d62206abea9f9ab5c9694c027e50
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-05 09:50:47 +0200

    DELETE: Ta bort en stad

--- Mer-om-fetch/index.js
+++ Mer-om-fetch/index.js
@@ -1,17 +1,7 @@
 let cityId = prompt("Input a city Id: ")
-let cityName = prompt("Input a city: ")
-let minPop = prompt("Input a min population: ")
-
-let city = {
-    "id": cityId,
-    "name": cityName, 
-    "population": parseInt(minPop)
-}
 
 fetch(`https://avancera.app/cities/${cityId}`, {
-  body: JSON.stringify(city),
-  headers: {'Content-Type': 'application/json'},
-  method: 'PUT'
+  method: 'DELETE'
+}).then(response => {
+  console.log(response)
 })
-  .then(response => response.json())
-  .then(result => {console.log(result)})
