5503dea7ea3e773fcf255796040ed590d0444cba
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-05 09:49:20 +0200

    PUT: Ändra namn och befolkning

--- Mer-om-fetch/index.js
+++ Mer-om-fetch/index.js
@@ -1,15 +1,17 @@
+let cityId = prompt("Input a city Id: ")
 let cityName = prompt("Input a city: ")
 let minPop = prompt("Input a min population: ")
 
 let city = {
+    "id": cityId,
     "name": cityName, 
     "population": parseInt(minPop)
 }
 
-fetch('https://avancera.app/cities/', {
+fetch(`https://avancera.app/cities/${cityId}`, {
   body: JSON.stringify(city),
   headers: {'Content-Type': 'application/json'},
-  method: 'POST'
+  method: 'PUT'
 })
   .then(response => response.json())
   .then(result => {console.log(result)})
