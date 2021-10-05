483df4b7b3201578a2a94b73e6cdbea4c8258720
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-05 10:07:03 +0200

    PATCH: Ändra namn och/eller befolkning

--- Mer-om-fetch/index.js
+++ Mer-om-fetch/index.js
@@ -1,14 +1,24 @@
 let cityId = prompt("Input a city Id: ")
 let cityName = prompt("Input a city: ")
+let cityPop = prompt("Input population: ")
+let city = {}
 
-let city = {
-    "name": cityName, 
+if (cityName) {
+    city.name = cityName
 }
-
-fetch(`https://avancera.app/cities/${cityId}`, {
-  body: JSON.stringify(city),
-  headers: {'Content-Type': 'application/json'},
-  method: 'PATCH'
-})
-  .then(response => response.json())
-  .then(result => {console.log(result)})
+if (cityPop) {
+    city.population = parseInt(cityPop)
+}
+if (cityId) {
+    fetch(`https://avancera.app/cities/${cityId}`, {
+            body: JSON.stringify(city),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'PATCH'
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+}
