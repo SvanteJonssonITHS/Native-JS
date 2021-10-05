61989e630e8ff2387c8e3937fcc83ba575ae52f6
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-05 09:52:12 +0200

    PATCH: Ändra namn

--- Mer-om-fetch/index.js
+++ Mer-om-fetch/index.js
@@ -1,7 +1,14 @@
 let cityId = prompt("Input a city Id: ")
+let cityName = prompt("Input a city: ")
+
+let city = {
+    "name": cityName, 
+}
 
 fetch(`https://avancera.app/cities/${cityId}`, {
-  method: 'DELETE'
-}).then(response => {
-  console.log(response)
+  body: JSON.stringify(city),
+  headers: {'Content-Type': 'application/json'},
+  method: 'PATCH'
 })
+  .then(response => response.json())
+  .then(result => {console.log(result)})
