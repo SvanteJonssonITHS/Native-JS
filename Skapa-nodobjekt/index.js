612fe14232173f000d891f1e3066824a7ca83679
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-07 10:30:18 +0200

    Ta bort ett element

--- Skapa-nodobjekt/index.js
+++ Skapa-nodobjekt/index.js
@@ -1,5 +1,3 @@
 let list = document.getElementsByTagName("ol")[0]
-let item = document.createElement("li")
-item.innerText = "Svante"
-
-list.appendChild(item)
+let item = document.getElementsByTagName("li")[1]
+list.removeChild(item)
