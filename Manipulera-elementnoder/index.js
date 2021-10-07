a30c8b5ac4dd1efebe457fd110301a3d24430300
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-07 10:18:40 +0200

    Lägg till HTML-innehåll i flera element

--- Manipulera-elementnoder/index.js
+++ Manipulera-elementnoder/index.js
@@ -1,1 +1,5 @@
-document.getElementsByTagName("li")[2].style.color = "#c00";
+let items = document.getElementsByTagName("li")
+
+for (let i = 0; i < items.length; i++) {
+    items[i].innerHTML += "<span class=\"credits\">100 credits</span>"    
+}
