e97bb66268778d2adca42893f1cecd1f7bb09a0b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-12 09:25:58 +0200

    Öka eller minska ett värde: Del 2

--- Events-och-addEventListener/index.js
+++ Events-och-addEventListener/index.js
@@ -1,9 +1,12 @@
 let element = document.getElementById('value')
 element.textContent = 1;
+document.getElementById('decrease').disabled = true
 
 document.getElementById('increase').addEventListener('click', () => {
     element.textContent = Number(element.textContent) + 1
+    if(element.textContent > 1) document.getElementById('decrease').disabled = false
 })
 document.getElementById('decrease').addEventListener('click', () => {
     element.textContent = Number(element.textContent) - 1
+    if(element.textContent < 2) document.getElementById('decrease').disabled = true
 })
