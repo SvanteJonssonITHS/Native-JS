2c09a366e8b2b7d423613e523d1914f0362a4601
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-12 09:58:04 +0200

    Öka eller minska ett värde: Del 3

--- Global-användning-av-addEventListener-samt-åtkomst-till-event-information/index.js
+++ Global-användning-av-addEventListener-samt-åtkomst-till-event-information/index.js
@@ -1,3 +1,24 @@
+let element = document.getElementById('value')
+element.textContent = 1;
+const decrease = document.getElementById('decrease')
+decrease.disabled = true
+
+document.getElementById('increase').addEventListener('click', () => {
+    element.textContent = Number(element.textContent) + 1
+    if (element.textContent > 1) decrease.disabled = false
+})
+document.getElementById('decrease').addEventListener('click', () => {
+    element.textContent = Number(element.textContent) - 1
+    if (element.textContent < 2) decrease.disabled = true
+})
+
 document.addEventListener('keydown', (e) => {
-    if(e.shiftKey && e.key != "Shift") {console.log(e.key)}
+    if (e.key == "ArrowUp") {
+        element.textContent = Number(element.textContent) + 1
+        if (element.textContent > 1) decrease.disabled = false
+    }
+    else if (e.key == "ArrowDown") {
+        if (element.textContent > 1) element.textContent = Number(element.textContent) - 1
+        if (element.textContent < 2) decrease.disabled = true
+    }
 })
