e8519d4c9a99f62272e36689bd22e5130a1640dc
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-14 10:32:43 +0200

    Städer

--- Web-Storage/index.js
+++ Web-Storage/index.js
@@ -1,2 +1,10 @@
-let a = [{name: "Alice"}]
-sessionStorage.setItem('users', JSON.stringify(a))
+let promise = fetch('https://avancera.app/cities/')
+
+promise
+  .then(response => {
+    let cities = response.json()
+    return cities
+  })
+  .then(result => {
+    localStorage.setItem('cities', JSON.stringify(result))
+  }) 
