72d0bf9b72606736ea77a49aad8de5f193d8a2e8
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-14 09:45:28 +0200

    Ett ogiltigt värde

--- Åtkomst-till-formulärvärden/index.js
+++ Åtkomst-till-formulärvärden/index.js
@@ -1,7 +1,26 @@
 let button = document.getElementById("send")
 let name = document.getElementById("name")
 let greeting = document.getElementById("greeting")
+let error = document.getElementById("errors")
+
+if(name.value.length > 0) {
+    greeting.innerText = `Hej ${name.value}!`
+    button.disabled = false
+} else {
+    error.style.display = "block"
+    button.disabled = true
+}
 
 button.addEventListener('click', () =>{
     greeting.innerText = `Hej ${name.value}!`
+})
+
+name.addEventListener('input', (e) =>{
+    if(e.target.value.length < 1) {
+        button.disabled = true;
+        error.style.display = "block"
+    } else {
+        button.disabled = false;
+        error.style.display = "none"
+    }
 })
